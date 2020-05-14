7f682cc2b6f1163bba11708722cc628dc0cf2494
arlette.valles <arlette.valles.contador@gmail.com>
2020-05-13 20:12:54 -0400

    identado js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-$(function(){
+$(function () {
 
-    $("a").click(function(event){
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var gato = this.hash;
-  
-        $("html, body").animate({
-          scrollTop: $(gato).offset().top
-        }, 800, function(){
-          window.location.hash = gato;
-        });
-      }
-    });
-  
-    $('[data-toggle="popover"]').popover();
-  
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function () {
+        window.location.hash = gato;
+      });
+    }
   });
-  
 
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+  $('[data-toggle="popover"]').popover();
+
+});
+
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
